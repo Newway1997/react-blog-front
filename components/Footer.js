022123dd42f123a8bb63d7749979b62d1ce5c438
Newway1997 <573491019@qq.com>
2020-03-26 10:56:57 +0800

    'beian'

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,7 @@ const Footer = () => (
   <div className="footer-div">
     <div>网站是由React+Egg+Ant Design构建的</div>
     <div>Newway</div>
+    <a href="http://www.beian.miit.gov.cn">渝ICP备20002393号</a>
   </div>
 );
 export default Footer;
